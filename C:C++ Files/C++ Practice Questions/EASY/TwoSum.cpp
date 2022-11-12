7bacf5b143857a5d2c0867c105da8e7058b22427
davidlacayo2 <davidlacayo2@gmail.com>
2022-11-12 12:05:06 -0600

    Finished a couple practice questions online

---
+++ C:C++ Files/C++ Practice Questions/EASY/TwoSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+vector<int> twoSum(vector<int> & nums, int target);
+
+int main(){
+
+}
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> temp;
+    for(int i = 0; i < nums.size()-1; i++) {
+        for(int j = i+1; j < nums.size(); j++) {
+            if(nums[i]+nums[j] == target){
+                temp.push_back(i);
+                temp.push_back(j);
+            }
+        }
+    }
+    return temp;
+}
