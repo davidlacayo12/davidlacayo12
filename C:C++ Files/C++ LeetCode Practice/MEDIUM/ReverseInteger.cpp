4eded00e44b1e6420f42fa1671597cbf8b5c2154
davidlacayo2 <davidlacayo2@gmail.com>
2023-06-20 17:48:24 -0500

    Final LeetCode for today

--- C:C++ Files/C++ LeetCode Practice/MEDIUM/ReverseInteger.cpp
+++ C:C++ Files/C++ LeetCode Practice/MEDIUM/ReverseInteger.cpp
@@ -4,11 +4,11 @@
 class Solution {
 public:
     int reverse(int x) {
-        int r=0;      //remainder variable
+        int r = 0;      //remainder variable
         while(x){
-         if (r>INT_MAX/10 || r<INT_MIN/10) return 0; //if r is outside 32-bit range then return 0 
-         r=r*10+x%10; // find remainder and add its to r
-         x=x/10;     // Update the value of x
+         if (r > INT_MAX / 10 || r < INT_MIN / 10) return 0; //if r is outside 32-bit range then return 0 
+         r = (r * 10) + (x % 10); // remainder calculation added to x
+         x = x / 10;    // Update the value of x
         } 
         return int(r);  // if r in the 32 bit range then return r
     }
