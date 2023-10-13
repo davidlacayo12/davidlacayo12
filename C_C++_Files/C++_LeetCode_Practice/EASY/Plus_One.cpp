b5e92b22cab7eadf098e56b6eceb814590b1f775
David Lacayo <davidlacayo2@gmail.com>
2023-10-13 09:42:21 -0500

    leetcode problem

---
+++ C_C++_Files/C++_LeetCode_Practice/EASY/Plus_One.cpp
@@ -0,0 +1,27 @@
+// Problem #66 in Leetcode
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >=0; i--) {
+            if(i == digits.size()-1) {
+                digits[i]++;
+            }
+            if(digits[i] == 10) {
+                digits[i] = 0;
+                if(i != 0) {
+                    digits[i-1]++;
+                } else {
+                    digits.push_back(0);
+                    digits[i] = 1;
+                }
+            }
+        }
+        return digits;
+    }
+};
