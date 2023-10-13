f0941dcce06b7dccdd1a7e71f14e9e0ad8404603
David Lacayo <davidlacayo2@gmail.com>
2023-10-13 11:25:42 -0500

    Adding interview python

---
+++ Python_Files/interview.py
@@ -0,0 +1,31 @@
+
+#x =[1,2,3,4,5,6,7]
+#x=[4:7]
+
+#y = [1,1,1,2,3,4,5,5,5,5]
+#new_y = []
+#for i in y:
+#    if(i not in new_y):
+#        new_y.append(i)
+#new_y = {}
+#for i in y:
+#    if(i not in new_y):
+#        new_y[i] = 1
+#    else:
+#        new_y[i] += 1
+
+
+#print(new_y)
+
+#str = "1.1.1.1"
+#print(str)
+
+#new_str = str.replace('.', '[.]')
+#print(new_str)
+
+#x = [None, 1, 2, None, 3, 4, None, 5, None, None]
+
+#for i in range(len(x)): 
+#    if x[i] == None: 
+#        x[i] = x[i-1]
+#print(x)
